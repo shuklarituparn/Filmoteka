5fb149075efd80af9a7a5a5db5ea15afbb8b600c
Rituparn Shukla <shukla.r@phystech.edu>
2024-03-18 23:53:06 +0300

    Added readme

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.17.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
-	github.com/ozontech/allure-go/pkg/framework v0.6.29
+	github.com/ozontech/cute v0.1.18
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.19.0
 	github.com/rs/cors v1.10.1
@@ -53,6 +53,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/ozontech/allure-go/pkg/allure v0.6.12 // indirect
+	github.com/ozontech/allure-go/pkg/framework v0.6.29 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.48.0 // indirect
@@ -60,6 +61,9 @@ require (
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
 	golang.org/x/net v0.21.0 // indirect
@@ -70,4 +74,5 @@ require (
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	moul.io/http2curl/v2 v2.3.0 // indirect
 )
--- test/integration_test/actor_controller_test.go
+++ test/integration_test/actor_controller_test.go
@@ -1,77 +1,59 @@
 package integration_test
 
-//import (
-//	"context"
-//	"encoding/json"
-//	"errors"
-//	"fmt"
-//	"github.com/ozontech/cute"
-//	"io"
-//	"net/http"
-//	"testing"
-//	"time"
-//)
-//
-//var requestbody = `{
-//		"email": "admin@example.com",
-//		"password": "adminpassword"
-//}`
-//
-//type Auth struct {
-//	Token string `json:"access_token"`
-//}
-//
-//var token Auth
-//var authTokenString string
-//
-//func TestExampleTest(t *testing.T) {
-//	getURL := fmt.Sprintf("http://localhost:8080/api/v1/actors/all?page=%d&page_size=%d", 1, 2)
-//	cute.NewTestBuilder().
-//		Title("Getting the actors").
-//		Tags("one_step", "some_local_tag", "json").
-//		Feature("some_feature").
-//		Description("some_description").
-//		CreateStep("SFFS").
-//		RequestBuilder(
-//			cute.WithMethod(http.MethodPost),
-//			cute.WithURI("http://localhost:8080/api/v1/users/login"),
-//			cute.WithHeaders(map[string][]string{
-//				"Content-Type": []string{"application/json"},
-//			}),
-//			cute.WithBody([]byte(requestbody)),
-//		).
-//		ExpectExecuteTimeout(10*time.Second).
-//		ExpectStatus(http.StatusOK).
-//		AssertResponse(
-//			func(resp *http.Response) error {
-//				if resp.ContentLength == 0 {
-//					return errors.New("content length is zero")
-//				}
-//				b, err := io.ReadAll(resp.Body)
-//				if err != nil {
-//					return err
-//				}
-//				if err := json.Unmarshal(b, &token); err != nil {
-//					fmt.Print(err)
-//				}
-//				fmt.Println(token.Token)
-//				authTokenString = fmt.Sprintf("Bearer %s", token.Token)
-//				return nil
-//
-//			},
-//		).
-//		NextTest().
-//		Create().
-//		RequestBuilder(
-//			cute.WithMethod(http.MethodGet),
-//			cute.WithURI(getURL),
-//			cute.WithHeaders(map[string][]string{
-//				"Authorization": []string{authTokenString},
-//			}),
-//		).
-//		ExpectStatus(http.StatusOK).
-//		ExecuteTest(context.Background(), t)
-//
-//	fmt.Println(authTokenString)
-//
-//}
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/ozontech/cute"
+	"io"
+	"net/http"
+	"testing"
+	"time"
+)
+
+var requestbody = `{
+		"email": "admin@example.com",
+		"password": "adminpassword"
+}`
+
+type Auth struct {
+	Token string `json:"access_token"`
+}
+
+var token Auth
+
+func TestUserLogin(t *testing.T) {
+	cute.NewTestBuilder().
+		Title("Checking user login").
+		Tags("user_login", "token", "auth").
+		Feature("Login").
+		Description("Testing if user can login into the system").
+		CreateStep("Login").
+		RequestBuilder(
+			cute.WithMethod(http.MethodPost),
+			cute.WithURI("https://api.rtprnshukla.ru/api/v1/users/login"),
+			cute.WithHeaders(map[string][]string{
+				"Content-Type": []string{"application/json"},
+			}),
+			cute.WithBody([]byte(requestbody)),
+		).
+		ExpectExecuteTimeout(10*time.Second).
+		ExpectStatus(http.StatusOK).
+		AssertResponse(
+			func(resp *http.Response) error {
+				if resp.ContentLength == 0 {
+					return errors.New("content length is zero")
+				}
+				b, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return err
+				}
+				if err := json.Unmarshal(b, &token); err != nil {
+					fmt.Print(err)
+				}
+				fmt.Println(token.Token)
+				return nil
+			},
+		).ExecuteTest(context.Background(), t)
+}
